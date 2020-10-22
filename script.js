f49c21bc1a2b1bcbe3295c3d74646897f55bf9c1
Kamil Korczynski <korczas+github@gmail.com>
2020-10-22 22:50:00 +0200

    pairing

--- script.js
+++ script.js
@@ -1,176 +1,182 @@
-const gameContainer = document.querySelector('.container')
-const startGameButton = document.querySelector('.game__start-button')
-const instructionPanels = document.querySelectorAll('.instruction')
-const pownImages = document.querySelectorAll('.pown__image')
-
+const gameContainer = document.querySelector(".container");
+const startGameButton = document.querySelector(".game__start-button");
+const instructionPanels = document.querySelectorAll(".instruction");
+const pawnImages = document.querySelectorAll(".pawn__image");
 
 let board;
-let pawns = []
-let player1Active = true;
-let currentPlayerId = 1
-let pownPlacementCounter = 0
-let moveAllow
-let gameStarted
-let gamePhase
+let pawns = [];
+let currentPlayerId = 0;
+let pawnPlacementCounter = 0;
+let gameStarted;
+let gamePhase;
+let choosenPawnId;
 
-const pownSorce = {
-    1: './icons/SVG/man-player1.svg',
-    2: './icons/SVG/man-player2.svg',
-    3: './icons/SVG/woman-player1.svg',
-    4: './icons/SVG/woman-player2.svg'
-}
+const pawnSorce = {
+  1: "./icons/SVG/man-player1.svg",
+  2: "./icons/SVG/man-player2.svg",
+  3: "./icons/SVG/woman-player1.svg",
+  4: "./icons/SVG/woman-player2.svg",
+};
 
 function changePlayerId() {
-    player1Active = !player1Active
-    player1Active ? currentPlayerId = 1 : currentPlayerId = 2
+  currentPlayerId = (currentPlayerId + 1) % 2;
 }
 
 function createBoard() {
-    let temp = []
-    for (let i = 0; i < 5; i++) {
-        temp.push([])
-        for (let j = 0; j < 5; j++) {
-            temp[i].push({ x: i, y: j, level: 0, dome: false, pawn: null })
-        }
+  let temp = [];
+  for (let i = 0; i < 5; i++) {
+    temp.push([]);
+    for (let j = 0; j < 5; j++) {
+      temp[i].push({ x: i, y: j, level: 0, dome: false, pawn: null });
     }
-    board = temp
+  }
+  board = temp;
 }
 
 function drawBoard() {
-    const boardHtml = board.map((i) => i.map((j) => `<div data-x="${j.x}" data-y="${j.y}" class="tile"></div>`).join('')).join('')
-    gameContainer.innerHTML = boardHtml
+  const boardHtml = board
+    .map((i) =>
+      i
+        .map((j) => `<div data-x="${j.x}" data-y="${j.y}" class="tile"></div>`)
+        .join("")
+    )
+    .join("");
+  gameContainer.innerHTML = boardHtml;
 }
 
 function updateBoard() {
-    const boardHtml = board.map((i) => i.map((j) => {
-        if (j.pawn === null) {
-            return `<div data-x="${j.x}" data-y="${j.y}" class="tile"></div>`
-        } else {
+  const boardHtml = board
+    .map((i) =>
+      i
+        .map((j) => {
+          if (j.pawn === null) {
+            return `<div data-x="${j.x}" data-y="${j.y}" class="tile"></div>`;
+          } else {
             return `<div data-x="${j.x}" data-y="${j.y}" class="tile">
-                <img class="game__pawn" id="${j.pawn.pawnId}" src="${pownSorce[j.pawn.pawnId]}" />
-            </div>`}
-    }).join('')).join('')
-    gameContainer.innerHTML = boardHtml
+                <img class="game__pawn" id="${j.pawn.pawnId}" src="${
+              pawnSorce[j.pawn.pawnId]
+            }" />
+            </div>`;
+          }
+        })
+        .join("")
+    )
+    .join("");
+  gameContainer.innerHTML = boardHtml;
 }
 
 function displayGameInstruction(playerId, gamePhase) {
-    let instruction
-    instructionPanels.forEach((panel) => panel.innerHTML = "")
-    switch (gamePhase) {
-        case 'placePawn':
-            instruction = "Place your pawn on an empty space of the board - click on choosen tile"
-            instructionPanels.forEach((panel) => panel.id == playerId ? panel.innerHTML = `<p>${instruction}</p>` : "")
-            break
-        case 'movePawn':
-            instruction = "Phase 1: building <br>pick up one pawn and make a move - click on choosen tile"
-            instructionPanels.forEach((panel) => panel.id == playerId ? panel.innerHTML = `<p>${instruction}</p>` : "")
-            break
-        case 'tileOccupated':
-            instruction = "THIS TILE IS ALREADY TAKEN - CHOOSE THE EMPTY ONE"
-            instructionPanels.forEach((panel) => panel.id == playerId ? panel.innerHTML = `<p>${instruction}</p>` : "")
-    }
+  let instruction;
+  switch (gamePhase) {
+    case "placePawn":
+      instruction =
+        "Place your pawn on an empty space of the board - click on choosen tile";
+      break;
+    case "movePawn":
+      instruction =
+        "Phase 1: building <br>pick up one pawn and make a move - click on choosen tile";
+      break;
+    case "tileOccupated":
+      instruction = "THIS TILE IS ALREADY TAKEN - CHOOSE THE EMPTY ONE";
+      break;
+  }
+  instructionPanels.forEach(
+    (panel) =>
+      (panel.innerHTML = panel.id == playerId ? `<p>${instruction}</p>` : "")
+  );
 }
 
-
-//phase0 pown placement on board
+//phase0 pawn placement on board
 function placePawns(event) {
-    const tile = event.target
-    if (!tile.classList.contains('tile')) return
-    checkIfMoveIsAllowed(tile)
-    if (!moveAllow) {
-        displayGameInstruction(currentPlayerId, 'tileOccupated')
+  const tile = event.target;
+  if (!tile.classList.contains("tile")) return;
+  if (!checkIfMoveIsAllowed(tile)) {
+    displayGameInstruction(currentPlayerId, "tileOccupated");
+  } else {
+    const { x, y } = tile.dataset;
+    const pawn = {
+      pawnId: pawnPlacementCounter,
+      postion: { x, y },
+      player: currentPlayerId,
+    };
+    pawns.push(pawn);
+    board[x][y].pawn = pawn;
+    updateBoard();
+    placedPawnHighlit();
+    if (pawns.length == 4) {
+      gameContainer.removeEventListener("click", placePawns);
+      gameContainer.removeEventListener("mousemove", highlitTile);
+      changePlayerId();
+      displayGameInstruction(currentPlayerId, "movePawn");
+      choosePawn();
+      gamePhase = "pawnMove";
     } else {
-        const { x, y } = tile.dataset
-        const pawn = { pawnId: pownPlacementCounter, postion: { x, y }, player: currentPlayerId }
-        pawns.push(pawn)
-        board[x][y].pawn = pawn
-        updateBoard()
-        placedPownHighlit()
-        if (pawns.length == 4) {
-            gameContainer.removeEventListener('click', placePawns)
-            gameContainer.removeEventListener('mousemove', highlitTile)
-            changePlayerId()
-            displayGameInstruction(currentPlayerId, 'movePawn')
-            choosePawn()
-            gamePhase = 'pawnMove'
-
-        } else {
-            changePlayerId()
-            displayGameInstruction(currentPlayerId, 'placePawn')
-        }
+      changePlayerId();
+      displayGameInstruction(currentPlayerId, "placePawn");
     }
+  }
 }
-//highlite ande remove pown from player container - use only in first phase - pown placement on board
-function placedPownHighlit() {
-    pownImages.forEach((image) => image.id == pownPlacementCounter ? image.classList.add("pown-used") : "")
-    pownPlacementCounter = pownPlacementCounter + 1
-    pownImages.forEach((image) => image.id == pownPlacementCounter ? image.classList.add("pown-active") : "")
+//highlite ande remove pawn from player container - use only in first phase - pawn placement on board
+function placedPawnHighlit() {
+  pawnImages.forEach((image) =>
+    image.id == pawnPlacementCounter ? image.classList.add("pawn-used") : ""
+  );
+  pawnPlacementCounter = pawnPlacementCounter + 1;
+  pawnImages.forEach((image) =>
+    image.id == pawnPlacementCounter ? image.classList.add("pawn-active") : ""
+  );
 }
-
 
 function choosePawn() {
-    const pawnsOnBoard = gameContainer.querySelectorAll('.game__pawn')
-    pawnsOnBoard.forEach((pawn) => pawn.addEventListener('click', highlitPawn))
+  const pawnsOnBoard = gameContainer.querySelectorAll(".game__pawn");
+  pawnsOnBoard.forEach((pawn) => pawn.addEventListener("click", highlitPawn));
 }
 
-
 function highlitPawn(e) {
-    const pawnsOnBoard = gameContainer.querySelectorAll('.game__pawn')
-    pawnsOnBoard.forEach((pawn)=>pawn.classList.remove('active__pawn'))
+  const pawnsOnBoard = gameContainer.querySelectorAll(".game__pawn");
+  pawnsOnBoard.forEach((pawn) => pawn.classList.remove("active__pawn"));
 
-    let choosenPawn
+  pawns.forEach((pawn) => {
+    if (pawn.pawnId == e.target.id && pawn.player == currentPlayerId) {
+      e.target.classList.add("active__pawn");
+      choosenPawnId = pawn.pawnId;
+    }
+  });
 
-
-       pawns.forEach((pawn)=>{
-        if(pawn.pawnId == e.target.id && pawn.player == currentPlayerId){
-        e.target.classList.add('active__pawn')
-        choosenPawn = pawn.postion
-       return choosenPawn}})
-
-    checkIfMoveIsAllowed(choosenPawn)
-
-
-
+  gamePhase = 'pawnMove'
 }
 
 function highlitTile(e) {
-    let activeTile = e.target
-    if (!activeTile.classList.contains('tile')) return
-    checkIfMoveIsAllowed(activeTile)
-    if (!moveAllow) return
-    activeTile.classList.add('active-tile')
-    activeTile.addEventListener('mouseleave', () => activeTile.classList.remove('active-tile'))
+  let activeTile = e.target;
+  if (!activeTile.classList.contains("tile")) return;
+  if (!checkIfMoveIsAllowed(activeTile)) return;
+  activeTile.classList.add("active-tile");
+  activeTile.addEventListener("mouseleave", () =>
+    activeTile.classList.remove("active-tile")
+  );
 }
 
 function checkIfMoveIsAllowed(selectedTile) {
-
-    //pown placement phase
-    switch (gamePhase) {
-        case 'pawnPlacement':
-            board[selectedTile.dataset.x][selectedTile.dataset.y].pawn == null ? moveAllow = true : moveAllow = false
-            //phase1 - move
-            break
-        case 'pawnMove':
-
-           board.forEach((tilesrow)=>tilesrow.forEach((tile)=>{
-
-
-           }
-           ))
+  const { x: tileX, y: tileY } = selectedTile.dataset;
+  //pawn placement phase
+  switch (gamePhase) {
+    case "pawnPlacement":
+      return board[tileX][tileY].pawn === null;
+    //phase1 - move
+    case "pawnMove":
+      const { x: pawnX, y: pawnY } = pawns[choosenPawnId];
+      // if ()
 
 
 
-            //x+1:  y-1 | y | y+1
-            //x:    y-1 |  y+1
-            //x11:  y-1 | y | y+1
-            // brak innego pionka board[selectedTile.dataset.x][selectedTile.dataset.y].pawn == null
+      //x+1:  y-1 | y | y+1
+      //x:    y-1 |  y+1
+      //x11:  y-1 | y | y+1
+      // brak innego pionka board[selectedTile.dataset.x][selectedTile.dataset.y].pawn == null
 
-        break
-    }
+      break;
+  }
 }
-
-
-
 
 // function movePawn(){
 //     dodaj komunikat dla graczy
@@ -189,38 +195,36 @@ function checkIfMoveIsAllowed(selectedTile) {
 // }
 
 function startGame() {
-    if (!gameStarted) {
-        gameStarted = true;
-        gamePhase = 'pawnPlacement'
-        startGameButton.innerHTML = "RE-START GAME"
-        createBoard()
-        drawBoard()
-        window.setTimeout(() => {
-            displayGameInstruction(currentPlayerId, 'placePawn')
-            placedPownHighlit()
-            gameContainer.addEventListener('mousemove', highlitTile)
-            gameContainer.addEventListener('click', placePawns)
-        }
-            , 200)
-    } else {
-        reStartGame()
-    }
+  if (!gameStarted) {
+    gameStarted = true;
+    gamePhase = "pawnPlacement";
+    startGameButton.innerHTML = "RE-START GAME";
+    createBoard();
+    drawBoard();
+    window.setTimeout(() => {
+      displayGameInstruction(currentPlayerId, "placePawn");
+      placedPawnHighlit();
+      gameContainer.addEventListener("mousemove", highlitTile);
+      gameContainer.addEventListener("click", placePawns);
+    }, 200);
+  } else {
+    reStartGame();
+  }
 }
 
 function reStartGame() {
-    pawns = []
-    player1Active = true;
-    gameStarted = false
-    pownPlacementCounter = 0
-    currentPlayerId = 1
-    moveAllow = true
-    gamePhase = ''
-    gameContainer.innerHTML = ""
-    pownImages.forEach((pawn) => {
-        pawn.classList.remove("pown-used")
-        pawn.classList.remove("pown-active")
-    })
-    startGameButton.innerHTML = "START GAME"
+  pawns = [];
+  player1Active = true;
+  gameStarted = false;
+  pawnPlacementCounter = 0;
+  currentPlayerId = 0;
+  gamePhase = "";
+  gameContainer.innerHTML = "";
+  pawnImages.forEach((pawn) => {
+    pawn.classList.remove("pawn-used");
+    pawn.classList.remove("pawn-active");
+  });
+  startGameButton.innerHTML = "START GAME";
 }
 
-startGameButton.addEventListener('click', startGame)
+startGameButton.addEventListener("click", startGame);
